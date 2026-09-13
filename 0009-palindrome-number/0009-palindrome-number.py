e7db2449cbbae5b18bce4e98a28adec07a99a473
Arnav Nigam <arnavnigamknp@gmail.com>
2026-09-13 23:58:34 +0530

    Time: 4 ms (81.24%), Space: 19.2 MB (87.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,15 @@
+# Python3 Solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Edge cases: negative numbers or numbers ending in 0 (except 0 itself)
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+            
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = (reversed_half * 10) + (x % 10)
+            x //= 10
+            
+        # For even length: x == reversed_half
+        # For odd length: x == reversed_half // 10 (gets rid of the middle digit)
+        return x == reversed_half or x == reversed_half // 10
